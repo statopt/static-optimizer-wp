9dba0d8e83e29ce82741784fac69adc717306ced
Slavi Marinov <slavi@orbisius.com>
2020-11-17 17:54:15 +0200

    added the link to open in a new window

--- modules/misc.php
+++ modules/misc.php
@@ -31,6 +31,7 @@ class StaticOptimizerMisc extends StaticOptimizerBase {
 		<div id="statopt_credits_wrapper" class="statopt_credits_wrapper">
 			<div id="statopt_credits_container" class="statopt_credits_container">
 				Static file optimization by <a href="https://statopt.com/?utm_source=plugin&utm_medium=credit"
+                                               target="_blank"
 				                               title="Optimizes and serves your assets (images, css, js) for faster site loading">StaticOptimizer</a>
 			</div>
 		</div> <!-- /statopt_credits_wrapper -->
